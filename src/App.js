21e8a30b3a527b32e46615607fb9979a28dd4b6d
huyhai1994 <nguyenhuyhai926@gmail.com>
2024-07-22 12:20:11 +0700

    finished function for CarSelector -> start css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import './App.css';
 import CarSelector from "./components/CarSelector";
+import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
     return (<CarSelector/>);
-
 }
 
 export default App;
--- src/components/CarSelector.js
+++ src/components/CarSelector.js
@@ -1,35 +1,49 @@
 import React, {useState} from 'react'
+import Form from 'react-bootstrap/Form';
+
 
 const CarSelector = () => {
-    const [car, setCar] = useState("");
-    const [color, setColor] = useState("");
     const [selectedCar, setSelectedCar] = useState("Volvo");
     const [selectedColor, setSelectedColor] = useState("Black");
-    return (<div>
-        <h1>select your car</h1>
-        <p>Select a car: </p>
-        <select
-            onChange={e => {
-                setSelectedCar(e.target.value);
-            }}
-        >
-            <option value="Volvo">Volvo</option>
-            <option value="Kia">Kia</option>
-            <option value="Honda Civic">Honda Civic</option>
-            <option value="SUV">SUV</option>
-        </select>
-        <p>Select a color: </p>
-        <select
-            onChange={e => {
-                setSelectedColor(e.target.value);
-            }}
-        >
-            <option value="Red">Red</option>
-            <option value="Black">Black</option>
-            <option value="Golden">Golden</option>
-            <option value="Blue">Blue</option>
-        </select>
-        <h2>Your selected: {selectedCar} {selectedColor} </h2>
+    return (<div className='container border rounded-3 shadow bg-light p-3'>
+        <h1 className='text-center'>select your car</h1>
+        <div className="row my-3">
+            <div className="col-sm-12
+             col-md-5">
+                <p>Select a car: </p>
+            </div>
+            <div className="col-sm-12">
+                <Form.Select
+                    onChange={e => {
+                        setSelectedCar(e.target.value);
+                    }}
+                >
+                    <option value="Volvo">Volvo</option>
+                    <option value="Kia">Kia</option>
+                    <option value="Honda Civic">Honda Civic</option>
+                    <option value="SUV">SUV</option>
+                </Form.Select>
+            </div>
+        </div>
+        <div className="row">
+            <div className=" col-sm-12">
+                <p>Select a color: </p>
+            </div>
+            <div className="col-sm-12">
+                <Form.Select
+                    onChange={e => {
+                        setSelectedColor(e.target.value);
+                    }}
+                >
+                    <option value="Red">Red</option>
+                    <option value="Black">Black</option>
+                    <option value="Golden">Golden</option>
+                    <option value="Blue">Blue</option>
+                </Form.Select>
+            </div>
+        </div>
+
+        <p className="mt-5 h4">Your selected: {selectedCar} {selectedColor} </p>
 
     </div>)
 }
